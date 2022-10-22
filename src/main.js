1f729d1f148ec8daa57f6c9966afb25d8c8d60ad
Michael Tew <mike.tew@gmail.com>
2022-10-22 03:00:40 -0700

    Improved styling.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-console.log('object');
+const rollDice = () => {
+  const numSides = document.getElementById('dice-selector').value
+  const randomRoll = Math.ceil(Math.random() * numSides)
+  document.getElementById('dice').innerHTML = randomRoll
+}
