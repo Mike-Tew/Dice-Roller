77eaa566425706fbafefaee306ee064a1133e0af
Michael Tew <mike.tew@gmail.com>
2022-10-22 03:22:29 -0700

    Implemented dice roll.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-const rollDice = () => {
+
+const setNumberOfSides = () => {
   const numSides = document.getElementById('dice-selector').value
   const randomRoll = Math.ceil(Math.random() * numSides)
   document.getElementById('dice').innerHTML = randomRoll
+}
+
+const rollDice = () => {
+
+  rollInterval = setInterval(() => {
+    setNumberOfSides()
+  }, 10);
+
+  setTimeout(() => {
+    clearInterval(rollInterval)
+  }, 2000);
 }
